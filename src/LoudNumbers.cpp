0a9eaf430bcc4b29ac5c8e6f015173199c0a96e4
Duncan Geere <radio.edit@gmail.com>
2022-04-19 12:14:02 +0200

    Adding dataviz and some other structural fixes

--- src/LoudNumbers.cpp
+++ src/LoudNumbers.cpp
@@ -3,42 +3,42 @@
 #include <vector>
 #include <osdialog.h>
 
-// Data variables
-std::vector<float> data{ 10.f,20.f,40.f,45.f,60.f,62.f,63.f,90.f };
-float datamin = *std::min_element(data.begin(), data.end());
-float datamax = *std::max_element(data.begin(), data.end());
-int row = 0;
-int datalength = static_cast<int>(data.size());
-
 // This function scales a number from one range to another
-float scalemap(float x, float inmin, float inmax, float outmin, float outmax) {
+float scalemap(float x, float inmin, float inmax, float outmin, float outmax)
+{
 	return outmin + (outmax - outmin) * ((x - inmin) / (inmax - inmin));
 };
 
-struct LoudNumbers : Module {
-	
-	enum ParamId {
+struct LoudNumbers : Module
+{
+
+	enum ParamId
+	{
 		RANGE_PARAM,
 		LENGTH_PARAM,
 		PARAMS_LEN
 	};
-	enum InputId {
+	enum InputId
+	{
 		TRIG_INPUT,
 		RESET_INPUT,
 		INPUTS_LEN
 	};
-	enum OutputId {
+	enum OutputId
+	{
 		MINUSFIVETOFIVE_OUTPUT,
 		ZEROTOTEN_OUTPUT,
 		VOCT_OUTPUT,
 		GATE_OUTPUT,
 		OUTPUTS_LEN
 	};
-	enum LightId {
+	enum LightId
+	{
 		LIGHTS_LEN
 	};
 
-	LoudNumbers() {
+	LoudNumbers()
+	{
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 		configParam(RANGE_PARAM, 1, 8, 2, "Octave range", " octaves");
 		getParamQuantity(RANGE_PARAM)->snapEnabled = true;
@@ -51,6 +51,17 @@ struct LoudNumbers : Module {
 		configOutput(GATE_OUTPUT, "Gate");
 	}
 
+	// Data variables
+	std::vector<float> data{0.f, 10.f, 20.f, 40.f, 45.f, 60.f, 62.f, 63.f, 10.f, 90.f, 100.f};
+	float datamin = *std::min_element(data.begin(), data.end());
+	float datamax = *std::max_element(data.begin(), data.end());
+	int row = -1; // because the first thing we do is increment it
+	int datalength = static_cast<int>(data.size());
+
+	// Style variables
+	std::string main = "#003380";
+	std::string faded = "#805279";
+
 	// Variables to track whether gate out is happening
 	bool outgateon = false;
 	bool ingateon = false;
@@ -60,41 +71,63 @@ struct LoudNumbers : Module {
 	// Trigger for incoming gate detection
 	dsp::SchmittTrigger ingate;
 
-	void process(const ProcessArgs& args) override {
+	void process(const ProcessArgs &args) override
+	{
 
 		// Logging some info about the data on first run.
-		if (firstrun) {
+		if (firstrun)
+		{
 			INFO("data min %f", datamin);
 			INFO("data max %f", datamax);
 			INFO("data length %i", datalength);
 			firstrun = false;
 		}
 
-		// if output gate is on
-		if (outgateon) {
+		// if output gate is on - CURRENTLY STUCK ON
+		if (outgateon)
+		{
 			// if wait time has elapsed
-			if (wait > params[LENGTH_PARAM].getValue()) {
+			if (wait > params[LENGTH_PARAM].getValue())
+			{
+				INFO("WAIT TIME ELAPSED");
 				// turn off the output gate
 				outputs[GATE_OUTPUT].setVoltage(0.f);
 				outgateon = false;
-			} else {
+				INFO("outgateon: false");
+			}
+			else
+			{
 				// If not, append to the wait variable
 				wait += args.sampleTime;
 			};
-		} else { // If the gate isn't on
-			
+		}
+		else
+		{ // If the gate isn't on
+
 			// and if a gate is high in the trigger input
-			if (ingate.process(inputs[TRIG_INPUT].getVoltage())) {
-			//if (inputs[TRIG_INPUT].getVoltage() > 0) {
+			if (ingate.process(inputs[TRIG_INPUT].getVoltage()))
+			{
+
+				// Increment the row number
+				row++;
+
+				// Check if row has hit max and loop if so
+				if (row == datalength)
+				{
+					row = 0;
+				}
 
 				// Calculate v/oct min and max
 				float voctmin;
 				float voctmax;
 
-				if (params[RANGE_PARAM].getValue() < 4) {
+				if (params[RANGE_PARAM].getValue() < 4)
+				{
 					voctmin = 0;
 					voctmax = params[RANGE_PARAM].getValue();
-				} else {
+				}
+				else
+				{
 					voctmin = 4 - params[RANGE_PARAM].getValue();
 					voctmax = 4;
 				}
@@ -105,37 +138,37 @@ struct LoudNumbers : Module {
 				outputs[VOCT_OUTPUT].setVoltage(scalemap(data[row], datamin, datamax, voctmin, voctmax));
 
 				// Logging for info
-				//INFO("row %d", row);
-				//INFO("datapoint %f", data[row]);
+				// INFO("row %d", row);
+				// INFO("datapoint %f", data[row]);
 
 				// Turn the gate on and reset the wait time
 				outputs[GATE_OUTPUT].setVoltage(10.f);
 				outgateon = true;
+				INFO("Outgateon: true");
 				wait = 0.f;
-
-				// Increment the row number
-				row++;
-
-			// Check if row has hit max and loop if so
-			if (row == datalength) {row = 0;};
 			}
 		}
 
 		// If a reset is received, reset the row count
-		if (inputs[RESET_INPUT].getVoltage() > 0) {row = 0;};
+		if (inputs[RESET_INPUT].getVoltage() > 0)
+		{
+			row = -1;
+		};
 	};
 
 	// Function to load a CSV file
-	void loadCSV() {
-		
+	void loadCSV()
+	{
+
 		// Default directory
 		std::string dir = asset::user("../");
 
 		// Get a path from the user
-		char* pathC = osdialog_file(OSDIALOG_OPEN, dir.c_str(), NULL, NULL);
+		char *pathC = osdialog_file(OSDIALOG_OPEN, dir.c_str(), NULL, NULL);
 
 		// If nothing gets chosen, don't do anything
-		if (!pathC) {
+		if (!pathC)
+		{
 			return;
 		}
 
@@ -145,59 +178,104 @@ struct LoudNumbers : Module {
 
 		// Then do what you want with the path.
 		processCSV(path);
-
 	}
 
 	// Do some stuff with the CSV
-	void processCSV(std::string path) {
+	void processCSV(std::string path)
+	{
 		INFO("test");
 	}
 };
 
-
 // This is the dataviz display
-struct DataViz : Widget {
+struct DataViz : Widget
+{
 
-	//box.size = mm2px(Vec(20.952, 15.594));
+	LoudNumbers *module; // NEW
+
 	const float margin = mm2px(2.0);
-	float width = box.size.x - 2 * margin;
-	float height = box.size.y - 2 * margin;
 
-	void drawLayer(const DrawArgs& args, int layer) override {
+	void drawLayer(const DrawArgs &args, int layer) override
+	{
+		float width = box.size.x - 2 * margin;
+		float height = box.size.y - 2 * margin;
+		float circ_size = module->datalength / 5;
+		// The API states that the module should only write to layer 1.
+		// And we don't want to run this until 'module' has actually been set.
+		if (layer == 1 && module)
+		{
 
-		// No idea what this does
-		if (layer != 1)
-			return;
-
-		// Loop over the datapoints
-		for (int d = 0; d < datalength; d++) {
-
-			// Calculate x and y coords
-			float x = d * width/datalength;
-			float y = scalemap(data[d], datamin, datamax, 0.f, height);
-
-			// Draw a circle for each
+			// Draw the line
 			nvgBeginPath(args.vg);
-			nvgCircle(args.vg, x, y, mm2px(4.0 / 2));
-			if (d == row) {
-				nvgFillColor(args.vg, color::alpha(color::WHITE, 1.f));
-			} else {
-				nvgFillColor(args.vg, color::alpha(color::YELLOW, 1.f));
+			nvgMoveTo(args.vg, 2 * margin, height - margin);
+
+			for (int d = 0; d < module->datalength; d++)
+			{
+
+				// Calculate x and y coords
+				float x = 2 * margin + (d * width / module->datalength);
+				// Y == zero at the TOP of the box.
+				float y = height - (margin + scalemap(module->data[d], module->datamin, module->datamax,
+													  0.f, height));
+
+				nvgLineTo(args.vg, x, y);
 			}
-			nvgFill(args.vg);
+
+			nvgStrokeColor(args.vg, color::fromHexString(module->faded));
+			nvgStrokeWidth(args.vg, mm2px(0.3));
+			nvgStroke(args.vg);
+			nvgClosePath(args.vg);
+
+			// Draw the circles
+			for (int d = 0; d < module->datalength; d++)
+			{
+
+				// Calculate x and y coords
+				float x = 2 * margin + (d * width / module->datalength);
+				// Y == zero at the TOP of the box.
+				float y = height - (margin + scalemap(module->data[d], module->datamin, module->datamax,
+													  0.f, height));
+
+				// Draw a circle for each
+				nvgBeginPath(args.vg);
+
+				if (d == module->row)
+				{
+					nvgCircle(args.vg, x, y, mm2px(1.f));
+					nvgFillColor(args.vg, color::fromHexString(module->main));
+				}
+				else if (d == 0 && module->row == module->datalength)
+				{
+					nvgCircle(args.vg, x, y, mm2px(1.f));
+					nvgFillColor(args.vg, color::fromHexString(module->main));
+				}
+				else
+				{
+					nvgCircle(args.vg, x, y, mm2px(circ_size / 2));
+					nvgFillColor(args.vg, color::fromHexString(module->faded));
+				}
+
+				nvgFill(args.vg);
+				nvgClosePath(args.vg);
+			}
 		}
+		Widget::drawLayer(args, layer);
 	}
 };
 
-struct LoadCSVItem : Widget {
-	LoudNumbers* module;
-	void onAction(const event::Action& e) override {
+struct LoadCSVItem : Widget
+{
+	LoudNumbers *module;
+	void onAction(const event::Action &e) override
+	{
 		module->loadCSV();
 	}
 };
 
-struct LoudNumbersWidget : ModuleWidget {
-	LoudNumbersWidget(LoudNumbers* module) {
+struct LoudNumbersWidget : ModuleWidget
+{
+	LoudNumbersWidget(LoudNumbers *module)
+	{
 		setModule(module);
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/LoudNumbers.svg")));
 
@@ -218,13 +296,15 @@ struct LoudNumbersWidget : ModuleWidget {
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(68.387, 96.195)), module, LoudNumbers::GATE_OUTPUT));
 
 		// mm2px(Vec(10.17, 10.174))
-		addChild(createWidget<LoadCSVItem>(mm2px(Vec(6.939, 15.594))));
+		// addChild(createWidget<LoadCSVItem>(mm2px(Vec(6.939, 15.594))));
 		// mm2px(Vec(53.817, 10.174))
-		addChild(createWidget<DataViz>(mm2px(Vec(20.952, 15.594))));
+		DataViz *data_viz = createWidget<DataViz>(mm2px(Vec(6.736, 29.813)));
+		data_viz->box.size = mm2px(Vec(67.832, 34.272));
+		data_viz->module = module;
+		addChild(data_viz);
 		// mm2px(Vec(67.83, 34.271))
-		addChild(createWidget<Widget>(mm2px(Vec(6.736, 29.813))));
+		// addChild(createWidget<Widget>(mm2px(Vec(20.952, 15.594))));
 	}
 };
 
-
-Model* modelLoudNumbers = createModel<LoudNumbers, LoudNumbersWidget>("LoudNumbers");
+Model *modelLoudNumbers = createModel<LoudNumbers, LoudNumbersWidget>("LoudNumbers");
